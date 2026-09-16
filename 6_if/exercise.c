2df2760ab4a7e8043254b5cb8c3b504f7eddf20c
Naimul Islam Mehedi <mehedi.interface@gmail.com>
2026-09-16 08:28:27 +0600

    if exercise

---
+++ 6_if/exercise.c
@@ -0,0 +1,25 @@
+// ### 🧪 Exercise 2: Check Adult Age
+// Write a C program that:
+// 1. Creates an integer variable:
+// int age = 20;
+
+// 2. Use `if` to check whether the age is **greater than or equal to 18**.
+
+// 3. If the condition is true, print:
+    // You are an adult.
+
+// ### Expected Output
+    // You are an adult.
+// **Rule:** Use only `if`. Don't use `else` yet.
+
+#include <stdio.h>
+
+int main() {
+    int age = 20; 
+
+    if (age >= 18) {
+        printf("You are an adult"); 
+    }
+
+    return 0; 
+}
---
+++ 6_if/if.c
@@ -0,0 +1,14 @@
+// The if statement is used when we want the program to make a decision 
+
+// Check if a number is positive 
+#include <stdio.h>
+
+int main() {
+    int number = 10; 
+
+    if (number > 0) {
+        printf("The number is positive."); 
+    }
+
+    return 0; 
+}
