7ea3fbc8ed16180a7d48a2e5ac26a68874d6ddec
Naimul Islam Mehedi <mehedi.interface@gmail.com>
2026-09-16 10:25:13 +0600

    if-else

---
+++ 7_if_else/if_else.c
@@ -0,0 +1,15 @@
+// if-else allows a program to make two possible decisions 
+
+#include <stdio.h>
+
+int main() {
+    int age = 16; 
+
+    if (age >= 18) {
+        printf("You are an adult.\n"); 
+    } else {
+        printf("You are not an adult.\n"); 
+    }
+
+    return 0; 
+}
---
+++ 7_if_else/positive_negative.c
@@ -0,0 +1,14 @@
+// Positive or NOt Positive 
+
+#include <stdio.h>
+
+int main() {
+    int number = -5; 
+
+    if (number > 0) {
+        printf("Number is positive.\n"); 
+    } else {
+        printf("Number is not positive.\n");    
+    }
+    return 0; 
+}
